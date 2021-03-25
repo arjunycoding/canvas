84aaa96b3cb774fcc14f16a0020f6e329af2725a
arjunyuvaraj <arjunycoding@gmail.com>
2021-03-24 20:33:57 -0400

    making it a full avatar maker website and adding styles

--- main.js
+++ main.js
@@ -9,39 +9,58 @@ function rect(x, y, width, height) {
     ctx.beginPath();
     ctx.fillRect(x, y, width, height);
 };
-
 function drawHuman(x, y){
-// draw face
-ctx.fillStyle = "chocolate";
-rect(x, y, 100, 100); // 20, 10
-// draw hair
-ctx.fillStyle = "black";
-ctx.beginPath();
-rect(x, y, 100, 20);
-ctx.fill();
-// draw neck
-ctx.fillStyle = "chocolate";
-rect(x * 3, y * 11, 20, 30);
-//draw shirt
-ctx.fillStyle = "red";
-rect(x, y * 14, 100, 150);
-// draw arms
-ctx.fillStyle = "chocolate";
-rect(x / 3.1, y * 14, 15, 100);
-rect(x * 6, y * 14, 15, 100);
-// draw legs
-ctx.fillStyle = "blue";
-rect(x * 5, y * 29, 20, 100);
-rect(x, y * 29, 20, 100);
-// draw eyes and mouth
-ctx.fillStyle = "black";
-circle(x * 2, y * 5, 10);
-circle(x * 5, y * 5, 10);
-ctx.fillStyle = "black";
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(x * 3.5, y * 6.5, 20, 0, Math.PI, false);
-ctx.fill();
+    var eye = document.getElementById("eyecolor").value;
+    var shirt = document.getElementById("shirtColor").value;
+    var skin = document.getElementById("skincolor").value;
+    var pant = document.getElementById("pantcolor").value;
+    // draw face
+    ctx.fillStyle = skin;
+    rect(x, y, 100, 100); // 20, 10
+    // draw hair
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    rect(x, y, 100, 20);
+    ctx.fill();
+    // draw neck
+    ctx.fillStyle = skin;
+    rect(x * 3, y * 11, 20, 30);
+    //draw shirt
+    ctx.fillStyle = shirt;
+    rect(x, y * 14, 100, 150);
+    // draw arms
+    ctx.fillStyle = skin;
+    rect(x / 3.1, y * 14, 15, 100);
+    rect(x * 6, y * 14, 15, 100);
+    // draw legs
+    ctx.fillStyle = pant;
+    rect(x * 5, y * 29, 20, 100);
+    rect(x, y * 29, 20, 100);
+    // draw eyes and mouth
+    ctx.fillStyle = eye;
+    circle(x * 2, y * 5, 10);
+    circle(x * 5, y * 5, 10);
+    ctx.fillStyle = "black";
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(x * 3.5, y * 6.5, 20, 0, Math.PI, false);
+    ctx.fill();
 }
-let pos = 20;
-drawHuman(pos, pos/2);
+
+function background(){
+// draw sky
+var sky = document.getElementById("skycolor").value;
+ctx.fillStyle = sky;
+rect(0, 0, 150, 1003);
+// draw grass
+ctx.fillStyle = "green";
+rect(0, 350, 150, 100);
+}
+function showHuman(){
+    console.log(canvas.id);
+    background();
+    drawHuman(20, 10);
+    ctx.clearRect();
+}
+canvas.id = "show";
+drawHuman(20, 10);
--- snowman.js
+++ snowman.js
@@ -6,18 +6,20 @@ function circle(x, y, radius) {
     ctx.arc(x, y, radius, 0, Math.PI * 2, false);
     ctx.fill();
 };
+function snowman(){
 let size = 5;
-// nose
+// head
 ctx.fillStyle = "white";
-circle(250, 145, 30);
+circle(250, 140, 30);
 ctx.stroke();
+// nose
 ctx.fillStyle = "orange";
 circle(250, 150, size);
 // eyes
 ctx.fillStyle = "black";
 circle(235, 135, size);
 circle(265, 135, size);
-// head
+// body
 ctx.fillStyle = "white";
 circle(250, 210, 40);
 ctx.stroke();
@@ -26,3 +28,4 @@ ctx.fillStyle = "black";
 circle(250, 190, size);
 circle(250, 205, size);
 circle(250, 220, size);
+}
