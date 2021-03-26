0d5284bc678e1f995fa655faec9d1a43433225f5
arjunyuvaraj <arjunycoding@gmail.com>
2021-03-25 21:20:11 -0400

    fixing the genirtater so that it will work for diffentert genders

--- main.js
+++ main.js
@@ -9,58 +9,116 @@ function rect(x, y, width, height) {
     ctx.beginPath();
     ctx.fillRect(x, y, width, height);
 };
-function drawHuman(x, y){
+function drawBoy(){
     var eye = document.getElementById("eyecolor").value;
     var shirt = document.getElementById("shirtColor").value;
     var skin = document.getElementById("skincolor").value;
     var pant = document.getElementById("pantcolor").value;
+    var hair = document.getElementById("haircolor").value;
     // draw face
     ctx.fillStyle = skin;
-    rect(x, y, 100, 100); // 20, 10
+    rect(20, 10, 100, 100); // 20, 10
     // draw hair
-    ctx.fillStyle = "black";
+    ctx.fillStyle = hair;
     ctx.beginPath();
-    rect(x, y, 100, 20);
+    rect(20, 10, 100, 20);
     ctx.fill();
     // draw neck
     ctx.fillStyle = skin;
-    rect(x * 3, y * 11, 20, 30);
+    rect(60, 110, 20, 30);
     //draw shirt
     ctx.fillStyle = shirt;
-    rect(x, y * 14, 100, 150);
+    rect(20, 140, 100, 150);
     // draw arms
     ctx.fillStyle = skin;
-    rect(x / 3.1, y * 14, 15, 100);
-    rect(x * 6, y * 14, 15, 100);
+    rect(5, 140, 15, 100);
+    rect(120, 140, 15, 100);
     // draw legs
     ctx.fillStyle = pant;
-    rect(x * 5, y * 29, 20, 100);
-    rect(x, y * 29, 20, 100);
+    rect(100, 290, 20, 100);
+    rect(20, 290, 20, 100);
     // draw eyes and mouth
     ctx.fillStyle = eye;
-    circle(x * 2, y * 5, 10);
-    circle(x * 5, y * 5, 10);
+    circle(40, 50, 10);
+    circle(100, 50, 10);
     ctx.fillStyle = "black";
     ctx.stroke();
     ctx.beginPath();
-    ctx.arc(x * 3.5, y * 6.5, 20, 0, Math.PI, false);
+    ctx.arc(70, 60, 20, 0, Math.PI, false);
     ctx.fill();
 }
-
+function drawGirl(){
+    var eye = document.getElementById("eyecolor").value;
+    var shirt = document.getElementById("shirtColor").value;
+    var skin = document.getElementById("skincolor").value;
+    var pant = document.getElementById("pantcolor").value;
+    var hair = document.getElementById("haircolor").value;
+    // draw hair part 2
+    ctx.fillStyle = hair;
+    ctx.beginPath();
+    rect(20, 10, 50, 200);
+    ctx.fill();
+    // draw face
+    ctx.fillStyle = skin;
+    rect(20, 10, 100, 100); // 20, 10
+    // draw hair part 1
+    ctx.fillStyle = hair;
+    ctx.beginPath();
+    rect(20, 10, 100, 20);
+    ctx.fill();
+    // draw neck
+    ctx.fillStyle = skin;
+    rect(60, 110, 20, 30);
+    //draw shirt
+    ctx.fillStyle = shirt;
+    rect(20, 140, 100, 150);
+    // draw arms
+    ctx.fillStyle = skin;
+    rect(5, 140, 15, 100);
+    rect(120, 140, 15, 100);
+    // draw legs
+    ctx.fillStyle = pant;
+    rect(100, 290, 20, 100);
+    rect(20, 290, 20, 100);
+    // draw eyes and mouth
+    ctx.fillStyle = eye;
+    circle(40, 50, 10);
+    circle(100, 50, 10);
+    ctx.fillStyle = "black";
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(70, 60, 20, 0, Math.PI, false);
+    ctx.fill();
+    // draw hair part 2
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    rect(20, 110, 40, 60);
+    // draw hair part 2
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    rect(80, 110, 40, 60);
+}
 function background(){
 // draw sky
 var sky = document.getElementById("skycolor").value;
-ctx.fillStyle = sky;
+ctx.fillStyle = "skyBlue";
 rect(0, 0, 150, 1003);
 // draw grass
 ctx.fillStyle = "green";
 rect(0, 350, 150, 100);
 }
 function showHuman(){
-    console.log(canvas.id);
-    background();
-    drawHuman(20, 10);
-    ctx.clearRect();
+    var gender = document.getElementById("gender").value;
+    if(gender == "girl"){
+        background();
+        drawGirl(20, 10);
+        console.log("gender");
+    } else {
+        background();
+        drawBoy(20, 10);
+        console.log("gendereeeeeeeeee");
+    }
+
 }
 canvas.id = "show";
-drawHuman(20, 10);
+drawBoy(20, 10);
